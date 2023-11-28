e75d13b039a94f5f43a7b15ac693c73356d99963
KevinRen <kevinren914@gmail.com>
2023-11-28 14:07:10 -0800

    Test Commit

--- CeeLoFinal/src/Game.java
+++ CeeLoFinal/src/Game.java
@@ -4,6 +4,8 @@ public class Game {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Poggers");
+		System.out.println("Gay");
+		System.out.print("Homo");
 	}
 
 }
