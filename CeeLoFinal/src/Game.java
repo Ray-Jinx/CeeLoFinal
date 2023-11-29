a157de75565221e924a2360571cccb24c2fa6239
Pham Hoang Nam <twinsquidz@gmail.com>
2023-11-28 17:49:41 -0800

    Removed Comment

--- CeeLoFinal/src/Game.java
+++ CeeLoFinal/src/Game.java
@@ -2,7 +2,6 @@
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Poggers");
 		System.out.println("Gay");
 		System.out.print("Homo");
